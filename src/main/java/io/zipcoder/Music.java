f8217e0e7638c671d80e6d83606d6f93bc149aa9
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-21 16:16:47 -0500

    started

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,23 @@ public class Music {
 
     private String[] playList;
 
+    //String[] n=songs | Integer k=index of the song | String q= the name of the song you wish to switch to
+
     public Music(String[] playList){
-        this.playList = playList;
+
+        //this.playList = playList;
+        Integer playListMoves = 0;
+
+        for(Integer k =1; k<playList.length; k++){
+            if(playList[k].equals(q)){
+                playListMoves++;
+            }
+        }
+        return null;
     }
 
     public Integer selection(Integer startIndex, String selection){
+
         return null;
     }
 }
