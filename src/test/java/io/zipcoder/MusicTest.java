32053bc79c42c392caff81063e212fc2b60fa6b0
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-02-21 22:37:05 -0500

    Garrett Finished Lab

--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -28,4 +28,51 @@ public class MusicTest {
         Integer actual = music.selection(startingIndex, selection);
         Assert.assertEquals(expected, actual);
     }
-}
+
+    @Test
+    public void forwardPlaylistCountTest1(){
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        String selection = "liveoak";
+        Integer expected = 2;
+        Integer actual = music.forwardPlaylistCount(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void forwardPlaylistCountTest2(){
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "dancinginthedark";
+        Integer expected = 3;
+        Integer actual = music.forwardPlaylistCount(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void backwardPlaylistCountTest1() {
+        String[] playlist = {"dancinginthedark", "rio", "liveoak", "liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        String selection = "liveoak";
+        Integer expected = 1;
+        Integer actual = music.backwardPlaylistCount(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+        @Test
+        public void backwardPlaylistCountTest2(){
+
+            String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+            Music music = new Music(playlist);
+            Integer startingIndex = 1;
+            String selection = "cecelia";
+            Integer expected = 2;
+            Integer actual = music.backwardPlaylistCount(startingIndex, selection);
+            Assert.assertEquals(expected, actual);
+        }
+    }
