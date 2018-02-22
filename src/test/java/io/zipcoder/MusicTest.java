7fd05426130f6e4760293132b598cf495121c4ec
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-21 19:36:21 -0500

    done

--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -2,13 +2,18 @@ package io.zipcoder;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.omg.PortableInterceptor.INACTIVE;
+import sun.rmi.server.InactiveGroupException;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MusicTest {
 
     @Test
-    public void selectionTest1(){
+    public void selectionTest1() {
 
-        String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        String[] playlist = {"wheniseeyouagain", "borntorun", "nothingelsematters", "cecelia"};
         Music music = new Music(playlist);
         Integer startingIndex = 1;
         String selection = "cecelia";
@@ -18,9 +23,30 @@ public class MusicTest {
     }
 
     @Test
-    public void selectionTest2(){
+    public void testMoveForwards() {
+        String[] playlist = {"wheniseeyouagain", "borntorun", "nothingelsematters", "cecelia"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 3;
+        Integer expected = 1;
+        Integer actual = music.moveForwards(startingIndex, "wheniseeyouagain");
+        Assert.assertEquals(expected, actual);
+    }
 
-        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+    @Test
+    public void testMoveBackwards() {
+        String[] playlist = {"wheniseeyouagain", "borntorun", "nothingelsematters", "cecelia"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        Integer expected = 3;
+        Integer actual = music.moveBackwards(startingIndex, "borntorun");
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void selectionTest2() {
+
+        String[] playlist = {"dancinginthedark", "rio", "liveoak", "liveoak"};
         Music music = new Music(playlist);
         Integer startingIndex = 0;
         String selection = "liveoak";
