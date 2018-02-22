ed976b30f0b1dca13bf32da8779acd6ca71e3950
Peter McCormick <petermccormick@zipcodersMBP109.fios-router.home>
2018-02-22 06:14:40 -0500

    taking a break

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.List;
+
 public class Music {
 
     private String[] playList;
@@ -8,7 +10,60 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        Integer[] occurrenceIndex = selectionOccurrences(selection);
+        int lowestPushesPressDown = pressDown(startIndex, occurrenceIndex);
+        int lowestPushesPressUp = pressUp(startIndex, occurrenceIndex);
+
+        if (lowestPushesPressUp <= lowestPushesPressDown) {
+            return lowestPushesPressUp;
+        } else return lowestPushesPressDown;
+    }
+
+
+    public Integer[] selectionOccurrences(String selection) {
+        Integer[] occurrenceIndex;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i =0; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                sb.append(i + " ";
+            }
+        }
+        return occurrenceIndex;
+
+    }
+
+    public int pressUp (Integer startIndex, Integer[] occurrenceIndex) {
+        int lowestPushes = 0;
+        for (int i = 0; i <occurrenceIndex.length; i ++ ) {
+            if (occurrenceIndex[i] > startIndex) {
+                if (lowestPushes <= (occurrenceIndex[i] - startIndex)) {
+                    lowestPushes = (occurrenceIndex[i] - startIndex);
+                }
+            }
+            if (occurrenceIndex[i] < startIndex) {
+                if ((Math.abs(0 - occurrenceIndex[i])) + (playList.length - startIndex) < lowestPushes) {
+                    lowestPushes = occurrenceIndex[i] - startIndex;
+                }
+            }
+        }
+        return lowestPushes;
+    }
+    public int pressDown (Integer startIndex, Integer[] occurrenceIndex) {
+        int lowestPushes = 0;
+        for (int i = 0; i <occurrenceIndex.length; i ++ ) {
+            if (occurrenceIndex[i] > startIndex) {
+                if (lowestPushes <= (occurrenceIndex[i] - startIndex)) {
+                    lowestPushes = (occurrenceIndex[i] - startIndex);
+                }
+            }
+            if (occurrenceIndex[i] < startIndex) {
+                if ((Math.abs(0 - occurrenceIndex[i])) + (playList.length - startIndex) < lowestPushes) {
+                    lowestPushes = occurrenceIndex[i] - startIndex;
+                }
+            }
+        }
+        return lowestPushes;
     }
 }
