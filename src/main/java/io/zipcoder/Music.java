1b0e8f4aec4079ca2cc3f2b022c21a4a9b359897
wes <wes.jones1597@gmail.com>
2019-10-24 16:26:10 -0400

    all tests pass. add some tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,7 +8,28 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int distance = 0;
+        int selectionIndex = 0;
+
+        for (int i = startIndex; i < playList.length+1; i++) {
+            distance++;
+            if (i >= playList.length) {
+                i = 0;
+            }
+            if (playList[i].equals(selection)) {
+                selectionIndex = i;
+            }
+            if (distance >= playList.length) break;
+        }
+
+        int dist1 = Math.abs(selectionIndex - startIndex);
+        int dist2 = startIndex + playList.length - selectionIndex;
+        if (dist1 > dist2) {
+            distance = dist2;
+        } else {
+            distance = dist1;
+        }
+        return distance;
     }
 }
