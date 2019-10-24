704ef7ba64996280b52f907d372002acee0a804a
Ryan <ryan@rmiller.dev>
2019-10-24 14:52:32 -0400

    Implement playlist

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,24 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int count = 0;
+        for (int i = startIndex; !playList[i].equals(selection); i++) {
+            if(i == playList.length - 1) {
+                i = -1;
+            }
+            count++;
+        }
+
+        int revCount = 0;
+        for (int i = startIndex; !playList[i].equals(selection); i--) {
+            if(i == 0) {
+                i = playList.length;
+            }
+            revCount++;
+        }
+
+        if(count < revCount) { return count; }
+        else { return revCount; }
     }
 }
