029de499a6a2d7bb1c09c09a64a941530cff741f
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-10-24 17:11:43 -0400

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,44 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int counter1 = 0;
+        int counter2 = 1;
+        int result = 0;
+
+        for (int x = startIndex; x < playList.length; x++){
+
+            if (playList[x].equals(selection) ){
+               break;
+            }
+
+            counter1++;
+        }
+
+        result = counter1;
+
+        if(startIndex == 0){
+
+            for (int x = playList.length - 1; x > 0; x--){
+
+             if (playList[x].equals(selection) ){
+                    break;
+                }
+
+             counter2++;
+
+             }
+
+              if(counter1 > counter2){
+                  result = counter2;
+              }else{
+                  result = counter1;
+              }
+        }
+
+
+
+
+        return result;
     }
 }
