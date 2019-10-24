6b2d4f0a426080fa0cc4baaa4d244953d8c90707
val <valfragier@gmail.com>
2019-10-24 16:09:54 -0400

    Lab complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,30 @@ public class Music {
 
     private String[] playList;
 
+    // constructor
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int counterA = 0;
+        int counterB = 0;
+
+
+        for(int i = 0; i <= startIndex; i++){
+            counterA++;
+        }
+        for(int j = 0; j <= playList.length; j++){
+            counterB++;
+        }
+        if(counterA > counterB){
+            // return the less skip song options
+            return counterB;
+        }
+        else{
+            return counterA;
+        }
+
+
     }
 }
