20b474b0bd487f33d26bf4f723f2de481e657985
Mike King <michkin32@gmail.com>
2019-10-25 15:54:57 -0400

    started

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,13 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
+
         return null;
     }
 }
+
