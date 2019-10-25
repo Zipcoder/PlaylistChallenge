376a9f33bab71a803b9f0ff479a88a2a3593369c
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-2.local>
2019-10-24 22:23:36 -0400

    mostly finished, but need to retool loops to avoid infinites

--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -28,4 +28,30 @@ public class MusicTest {
         Integer actual = music.selection(startingIndex, selection);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void selectionTest3(){
+
+        String[] playlist = {"heyjude","inmylife","letitbe","ijustseenaface",
+                "isawherstandingthere","tickettoride","help","paperbackwriter","harddaysnight"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 2;
+        String selection = "tickettoride";
+        Integer expected = 3;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void selectionTest4(){
+
+        String[] playlist = {"bohemianrhapsody","killerqueen","fatbottomedgirls","underpressure",
+                "iwantobreakfree","somebodytolove","flashgordontheme","crazylittlethingcalledlove","wearethechampions"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "crazylittlethingcalledlove";
+        Integer expected = 3;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
 }
