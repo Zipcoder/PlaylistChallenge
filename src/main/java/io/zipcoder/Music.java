fe37d32030c76ffa2dbebbf3a904bf8536cef08c
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-10-26 14:16:58 -0400

    playList done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,46 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+
+
+    public Integer selection(Integer startIndex, String selection) {
+
+
+
+        int leftCount=0;
+        int rightCount=0;
+        //right counting
+        int i=startIndex;
+        while(playList[i]!=selection){
+            i++;
+            rightCount++;
+            if(i==playList.length){
+                i=0;
+            }
+
+        }
+        //left counting
+        int j=startIndex;
+        while(playList[j]!=selection){
+            j--;
+            leftCount++;
+
+            if(j<0){
+                j=playList.length-1;
+            }
+
+        }
+        if(leftCount<=rightCount){
+            return leftCount;
+        }
+
+
+
+
+        return rightCount;
     }
 }
