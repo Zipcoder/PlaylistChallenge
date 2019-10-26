b4fcc7f4aac918026bc79e94c29afa433cab3dbf
Mike King <michkin32@gmail.com>
2019-10-26 14:07:35 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -10,8 +10,35 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
+        Integer upButton = pressUpButton(startIndex);
+        Integer downButton = pressDownButton(startIndex);
 
-        return null;
+        if (upButton < downButton){
+            return upButton;
+        }
+
+        return downButton;
+    }
+
+    private Integer pressDownButton(Integer startIndex) {
+        Integer secondCount = 0;
+        for (int i = startIndex; i != playList.length - 1; i--){
+            if (i == 0){
+                i = playList.length;
+                secondCount++;
+            }else{
+            secondCount++;
+        }
+        }
+        return secondCount;
+    }
+
+    private Integer pressUpButton(Integer startIndex) {
+        Integer count = 0;
+        for (int i = startIndex; i < playList.length; i++){
+            count++;
+        }
+        return count;
     }
 }
 
