2cac9a96699b1ba5b315c76e1c2f0842db4b1f50
MannyMonocle <eetumadu@gmail.com>
2019-10-29 17:29:14 -0400

    completed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +11,57 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        String[] upButton = new String[playList.length];
+        String[] downButton = new String[playList.length];
+
+
+        // build downButton array
+
+        int w = 0;
+        for (int i = startIndex; i < playList.length; i++){
+            downButton[w] = playList[i];
+            w++;
+        }
+
+        //int w = playList.length - startIndex;
+
+        for (int i = 0; w < playList.length; i++) {
+            downButton[w] = playList[i];
+            w++;
+        }
+
+
+
+        // build upButton array
+
+        int x = 0;
+        for (int j = startIndex; j >= 0; j--){
+            upButton[x] = playList[j];
+            x++;
+        }
+
+        for(int j = playList.length -1; j > startIndex; j--){
+            upButton[x] = playList[j];
+            x++;
+        }
+
+        //print arrays
+        /*
+        System.out.println(Arrays.toString(upButton));
+        System.out.println(Arrays.toString(downButton));
+        */
+
+        //test which index is lower
+
+        int ans;
+
+        if(Arrays.asList(downButton).indexOf(selection) <= Arrays.asList(upButton).indexOf(selection)){
+            ans = Arrays.asList(downButton).indexOf(selection);
+        }   else {
+            ans = Arrays.asList(upButton).indexOf(selection);
+        }
+
+        return ans;
     }
 }
