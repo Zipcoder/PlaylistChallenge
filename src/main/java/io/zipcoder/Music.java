9f4b2c3d40aa4779866f5af75c286758fc87383b
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-14 08:59:05 -0500

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,33 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+//    String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+//    Music music = new Music(playlist);
+//    Integer startingIndex = 0;
+//    String selection = "liveoak";
+//    Integer expected = 1;
+//    Integer actual = music.selection(startingIndex, selection);
+//        Assert.assertEquals(expected, actual);
+
+//    def distance(len_my_list, idx_1, idx_2):
+//    dist = abs(idx_1 - idx_2)
+//    return min(len_my_list - dist, dist)
+    public Integer selection(Integer startIndex, String selection) {
+        int current = 0;
+
+
+        for (int i = 0; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                 current = Math.abs(startIndex - i);
+
+            }
+        }
+
+
+        return Integer.min(playList.length - current, current);
     }
 }
