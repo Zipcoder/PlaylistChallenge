c2824c6582615bebddbd542feb55cf3aa06b12ed
chipfody <60615532+chipfody@users.noreply.github.com>
2020-02-16 20:30:45 -0500

    Played out

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,35 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+
+        Integer search = 0;
+        Integer upCount;
+        Integer downCount;
+        Integer endOfArray = playList.length;
+
+        if (startIndex == endOfArray) {upCount = 0;}
+            else {upCount = startIndex +1;}
+        if (startIndex == 0) {downCount = endOfArray - 1;}
+            else {downCount = startIndex -1;}
+
+
+        if (playList[startIndex] == selection) {
+            //return search;
+        } else {search = 1;
+            while (playList[upCount] != selection && playList[downCount] != selection) {
+                //search++;
+                if (upCount == endOfArray - 1) {upCount = 0;} else {upCount++;}
+                if (downCount == 0) {downCount = endOfArray - 1;} else {downCount--;}
+                search++;
+            }
+        }
+
+        return search;
+
     }
 }
